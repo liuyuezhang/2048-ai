c25931fcca3b2ee5d9e25b6b558e29a98039a0af
liuyuezhangadam <liuyuezhangadam@gmail.com>
2018-04-06 17:24:22 +0800

    2048. minimax + heuristic

--- PlayerAI_3.py
+++ PlayerAI_3.py
@@ -1,8 +1,86 @@
-from random import randint
 from BaseAI_3 import BaseAI
+from copy import deepcopy
+from my_2048_operation import *
+
+# monotonicity
+weight = [[pow(3, 3),  pow(3, 2),  pow(3, 1),  pow(3, 0)],
+          [pow(3, 4),  pow(3, 5),  pow(3, 6),  pow(3, 7)],
+          [pow(3, 11), pow(3, 10), pow(3, 9),  pow(3, 8)],
+          [pow(3, 12), pow(3, 13), pow(3, 14), pow(3, 15)]]
+
+# smoothness
+k = 1
+
+
+# heuristic function
+def heuristic(state):
+    reward = 0
+    max_tile = 0
+    for i in range(4):
+        for j in range(4):
+            # monotonicity
+            reward += weight[i][j] * state[i][j]
+            # smoothness
+            if i - 1 >= 0:
+                reward -= k * abs(state[i][j] - state[i - 1][j])
+            if i + 1 < 4:
+                reward -= k * abs(state[i][j] - state[i + 1][j])
+            if j - 1 >= 0:
+                reward -= k * abs(state[i][j] - state[i][j - 1])
+            if j + 1 < 4:
+                reward -= k * abs(state[i][j] - state[i][j + 1])
+            # empty tiles
+            if state[i][j] == 0:
+                reward += 2
+            # find max tile
+            if state[i][j] > max_tile:
+                max_tile = state[i][j]
+    # avoid max tile leaving
+    if state[3][3] != max_tile and state[3][3] != 0:
+        reward -= state[3][3] * weight[3][3]
+
+    return reward
+
+
+# minimax search with alpha beta pruning
+def minimax(state, depth, alpha, beta, is_max):
+    if depth == 0:
+        return heuristic(state)
+    if is_max:
+        v = float('-inf')
+        for i in range(4):
+            new_state = deepcopy(state)
+            if move(new_state, i):
+                v = max(v, minimax(new_state, depth-1, alpha, beta, False))
+                alpha = max(alpha, v)
+                if beta < alpha:
+                    break
+        return v
+    else:
+        v = float('inf')
+        for cell in find_empty(state):
+            new_state = deepcopy(state)
+            new_state[cell[0]][cell[1]] = 2
+            v = min(v, minimax(new_state, depth-1, alpha, beta, True))
+            beta = min(beta, v)
+            if beta < alpha:
+                break
+        return v
 
 
 class PlayerAI(BaseAI):
     def getMove(self, grid):
-        moves = grid.getAvailableMoves()
-        return moves[randint(0, len(moves) - 1)] if moves else None
+        state = grid.map
+
+        # be aware of deepcopy and []*4
+        states = [deepcopy(state), deepcopy(state), deepcopy(state), deepcopy(state)]
+        moved = [False]*4
+        rewards = [-float("inf")]*4
+
+        # 0: UP, 1:DOWN, 2:LEFT, 3:RIGHT
+        for i in range(4):
+            moved[i] = move(states[i], i)
+            if moved[i]:
+                rewards[i] = minimax(states[i], 3, float('-inf'), float('inf'), False)
+
+        return rewards.index(max(rewards))
---
+++ my_2048_operation.py
@@ -0,0 +1,175 @@
+from random import randint
+
+directionVectors = (UP_VEC, DOWN_VEC, LEFT_VEC, RIGHT_VEC) = ((-1, 0), (1, 0), (0, -1), (0, 1))
+vecIndex = [UP, DOWN, LEFT, RIGHT] = range(4)
+
+
+# Move
+# Merge Tiles
+def merge(cells):
+    if len(cells) <= 1:
+        return cells
+
+    i = 0
+
+    while i < len(cells) - 1:
+        if cells[i] == cells[i+1]:
+            cells[i] *= 2
+
+            del cells[i+1]
+
+        i += 1
+
+
+def move_up(state):
+    r = range(4)
+
+    moved = False
+
+    for j in range(4):
+        cells = []
+
+        for i in r:
+            cell = state[i][j]
+
+            if cell != 0:
+                cells.append(cell)
+
+        merge(cells)
+
+        for i in r:
+            value = cells.pop(0) if cells else 0
+
+            if state[i][j] != value:
+                moved = True
+
+            state[i][j] = value
+
+    return moved
+
+
+def move_down(state):
+    r = range(3, -1, -1)
+
+    moved = False
+
+    for j in range(4):
+        cells = []
+
+        for i in r:
+            cell = state[i][j]
+
+            if cell != 0:
+                cells.append(cell)
+
+        merge(cells)
+
+        for i in r:
+            value = cells.pop(0) if cells else 0
+
+            if state[i][j] != value:
+                moved = True
+
+            state[i][j] = value
+
+    return moved
+
+
+def move_left(state):
+    r = range(4)
+
+    moved = False
+
+    for i in range(4):
+        cells = []
+
+        for j in r:
+            cell = state[i][j]
+
+            if cell != 0:
+                cells.append(cell)
+
+        merge(cells)
+
+        for j in r:
+            value = cells.pop(0) if cells else 0
+
+            if state[i][j] != value:
+                moved = True
+
+            state[i][j] = value
+
+    return moved
+
+
+def move_right(state):
+    r = range(3, -1, -1)
+
+    moved = False
+
+    for i in range(4):
+        cells = []
+
+        for j in r:
+            cell = state[i][j]
+
+            if cell != 0:
+                cells.append(cell)
+
+        merge(cells)
+
+        for j in r:
+            value = cells.pop(0) if cells else 0
+
+            if state[i][j] != value:
+                moved = True
+
+            state[i][j] = value
+
+    return moved
+
+
+def move(state, i):
+    if i == 0:
+        return move_up(state)
+    if i == 1:
+        return move_down(state)
+    if i == 2:
+        return move_left(state)
+    if i == 3:
+        return move_right(state)
+
+
+# Put
+# Return All the Empty Cells
+def find_empty(state):
+    cells = []
+
+    for x in range(4):
+        for y in range(4):
+            if state[x][y] == 0:
+                cells.append((x, y))
+
+    return cells
+
+
+# Return the Tile with Maximum Value
+def find_max(state):
+    res = 0
+
+    for x in range(4):
+        for y in range(4):
+            res = max(res, state[x][y])
+
+    return res
+
+
+def out_bound(pos):
+    return pos[0] < 0 or pos[0] >= 4 or pos[1] < 0 or pos[1] >= 4
+
+
+def put(state):
+    cells = find_empty(state)
+    if len(cells) == 0:
+        return state, False
+    cells[randint(0, len(cells) - 1)]
